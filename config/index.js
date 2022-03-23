89674c6a24786f5168abfda9601c6b97d9a49b42
jack-weng <1280994670@qq.com>
2022-03-23 22:25:53 +1100

    Fix domain

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://vercel-familiarisation-exercise.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'vercel-familiarisation-exercise-g01lck16a-jack-weng.vercel.app'
