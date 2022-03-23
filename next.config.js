7b7cddd57b98b71457eaf819f61fbbbe2c9e0bac
jack-weng <1280994670@qq.com>
2022-03-23 22:54:00 +1100

    Fix redirect

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  redirects: [
-    { 
-      source: '/hello-vercel',
-      destination: 'https://vercel.com',
-      permanent: true,
-    }
-  ]
+// const nextConfig = {
+//   reactStrictMode: true,
+//   redirects: [
+//     { 
+//       source: '/hello-vercel',
+//       destination: 'https://vercel.com',
+//       permanent: true,
+//     }
+//   ]
+// }
+
+module.exports = {
+  async redirects() {
+    return [
+      { 
+        source: '/hello-vercel',
+        destination: 'https://vercel.com',
+        permanent: false,
+      }
+    ]
+  },
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
